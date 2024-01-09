85ee6381bfa9b1d1e1eeb5e9d56548abcf2e5285
austinc512 <cello.austin1994@gmail.com>
2024-01-08 23:33:37 -0600

    added some comments

--- src/state/reducers/PKGsReducer.ts
+++ src/state/reducers/PKGsReducer.ts
@@ -27,13 +27,16 @@ const PKGsReducer = (
   switch (action.type) {
     case ActionType.SEARCH_PKGS:
       // 100% certain that 'action' is SearchPKGsAction
+      // the action object matches the structure defined by the interface.
       return { loading: true, error: null, data: [] };
     case ActionType.SEARCH_PKGS_SUCCESS:
       // 100% certain that 'action' is SearchPKGsSuccessAction
+      // the action object matches the structure defined by the interface.
       return { loading: false, error: null, data: action.payload };
     case ActionType.SEARCH_PKGS_ERROR:
       return { loading: false, error: action.payload, data: [] };
     // 100% certain that 'action' is SearchPKGsErrorAction
+    // the action object matches the structure defined by the interface.
     default:
       return state;
   }
